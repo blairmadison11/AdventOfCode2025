a0e7762082d134171319e6ada0329db3df0161b6
Blair Durkee <blairmadison11@gmail.com>
2025-12-07 02:28:43 -0500

    Create Day07_Part2.cs

---
+++ Day07_Part2.cs
@@ -0,0 +1,18 @@
+var grid = File.ReadAllLines("input.txt").Select(l => l.ToCharArray()).ToArray();
+var beams = new Dictionary<int,ulong>() { { Array.IndexOf(grid[0], 'S'), 1} };
+var lastpos = grid[0].Length - 1;
+for (int i = 1; i < grid.Length; ++i)
+{
+    var nextBeams = new Dictionary<int,ulong>(beams);
+    foreach (var beam in beams.Keys)
+    {
+        if (grid[i][beam] == '^')
+        {
+            nextBeams[beam - 1] = nextBeams.GetValueOrDefault(beam - 1) + nextBeams[beam];
+            nextBeams[beam + 1] = nextBeams.GetValueOrDefault(beam + 1) + nextBeams[beam];
+            nextBeams[beam] = 0;
+        }
+    }
+    beams = nextBeams;
+}
+Console.WriteLine(beams.Values.Aggregate((ulong)0, (a, c) => a + c));
