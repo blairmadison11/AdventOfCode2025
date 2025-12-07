d399d2574b5b66d22428b8ddd224bf7e5f0115a2
Blair Durkee <blairmadison11@gmail.com>
2025-12-07 02:32:39 -0500

    Update Day07_Part1.cs

--- Day07_Part1.cs
+++ Day07_Part1.cs
@@ -1,23 +1,22 @@
 var grid = File.ReadAllLines("input.txt").Select(l => l.ToCharArray()).ToArray();
 var beams = new HashSet<int>() { Array.IndexOf(grid[0], 'S') };
-var lastpos = grid[0].Length - 1;
-long splits = 0;
+ulong splits = 0;
 for (int i = 1; i < grid.Length; ++i)
 {
-    var newBeams = new HashSet<int>();
+    var nextBeams = new HashSet<int>();
     foreach (var beam in beams)
     {
         if (grid[i][beam] == '^')
         {
-            if (beam > 0) newBeams.Add(beam - 1);
-            if (beam < lastpos) newBeams.Add(beam + 1);
+            nextBeams.Add(beam - 1);
+            nextBeams.Add(beam + 1);
             ++splits;
         }
         else
         {
-            newBeams.Add(beam);
+            nextBeams.Add(beam);
         }
     }
-    beams = newBeams;
+    beams = nextBeams;
 }
 Console.WriteLine(splits);
